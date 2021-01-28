734d73516140e4da16a2552416d1590b3fb772c3
Couapy <mael@marchand.cloud>
2021-01-28 19:17:11 +0100

    feat: add handler for weapons

--- src/hypex-core/src/main/java/cloud/marchand/hypex/core/enumerations/WeaponState.java
+++ src/hypex-core/src/main/java/cloud/marchand/hypex/core/enumerations/WeaponState.java
@@ -1,8 +1,25 @@
 package cloud.marchand.hypex.core.enumerations;
 
+/**
+ * Indicates the weapon status.
+ */
 public enum WeaponState {
-    ready,
-    firing,
-    reloading,
-    empty
+    /**
+     * The weapon is loading.
+     * Happens when the player select the weapon.
+     */
+    LOADING,
+    /**
+     * The weapon is ready to fire ({@link #FIRING}), or to reload ({@link #RELOADING}).
+     */
+    READY,
+    /**
+     * The weapon is reloading.
+     */
+    FIRING,
+    /**
+     * The weapon is reloading.
+     * It will come back to {@link #READY}.
+     */
+    RELOADING
 }
